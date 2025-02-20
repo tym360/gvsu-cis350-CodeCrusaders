a56124f3f905f2833535e124c90ac86e9e4fac5c
Hunter Turnbull <turnbuhu@mail.gvsu.edu>
2025-02-19 20:14:50 -0500

    Added different difficulties in word_generator file. Also adjust lingo_legends_framework file so that instead of word length being assumed to always be five, it uses the length of the generated word. It also shows the letter as red when the letter isn't in the word instead of just being an underscore.

--- src/LingoLegendsFramework.py
+++ src/LingoLegendsFramework.py
@@ -7,45 +7,44 @@ TODO:
 """
 
 import random
-from colorama import Style, Fore;
-import word_generator;
-#Will change to API when chosen
-# words = ["tiger", "bacon", "agile", "group", "lingo"]
-# Level_word = random.choice(words)
-Level_word = word_generator.generate_word()
+from colorama import Style, Fore
+import word_generator
+
+
+Level_word = word_generator.generate_word_medium()
 
 print(f"Lingo legends {Fore.GREEN}framework{Style.RESET_ALL}")
-print("You will be given 5 attempts to guess the word (5 Letters)")
+print(f"You will be given 5 attempts to guess the word ({len(Level_word)} Letters)")
 
 User_attempts = 0
 winner = False
 while User_attempts < 5:
     Current_attempt = input("Guess " +  str((User_attempts + 1)) + " >>").lower()
-    #make sure sure gives the right amount of letters
+    # Make sure user gives the right amount of letters
 
     Progress = ""
-    #Exception handling Below:  Current_Attempt is broken down into an array, something outside of Level_word's length will cause an Index Error. 
-    #Test parsing
+    # Exception handling Below:  Current_Attempt is broken down into an array, something outside of Level_word's length will cause an Index Error.
+    # Test parsing
     try:
-        for x in range(5):
+        for x in range(len(Level_word)):
             if Current_attempt[x] == Level_word[x]:
-                Progress += Fore.GREEN + Current_attempt[x]+ Style.RESET_ALL
+                Progress += Fore.GREEN + Current_attempt[x] + Style.RESET_ALL
             elif Current_attempt[x] in Level_word:
-                Progress += Fore.YELLOW + Current_attempt[x]+ Style.RESET_ALL
+                Progress += Fore.YELLOW + Current_attempt[x] + Style.RESET_ALL
             else:
-                Progress += "_"
+                Progress += Fore.RED + Current_attempt[x] + Style.RESET_ALL
         print("Your progress: " + Progress.strip())
-    #If an IndexError occurs, we will let the user retry the attempt
+    # If an IndexError occurs, we will let the user retry the attempt
     except IndexError:
-        print("Please enter a word that is 5 letters long")
+        print(f"Please enter a word that is {len(Level_word)} letters long")
         User_attempts = User_attempts - 1
     User_attempts = User_attempts + 1
 
-    
 
     if Current_attempt == Level_word:
         print("Yes! the word was: " + Fore.GREEN + Level_word + Style.RESET_ALL)
         winner = True
         break
+
 if(winner != True):
     print("You have used all of your attempts! The correct word was: " + Fore.GREEN + Level_word + Style.RESET_ALL)
--- src/word_generator.py
+++ src/word_generator.py
@@ -1,11 +1,22 @@
+"""
+-Need to find ways to adjust difficulty of words generated
+    -Could potentially limit easy to one type of speech or make it possible to generate four-letter words.
+    -Easier and medium could potentially exclude "rarer" letter like x, q, etc.
+    -Harder could also generate longer words and could always have one of a few different "rarer" letter like x, q, etc.
+"""
 import wonderwords
 from wonderwords import RandomWord
 
 r = RandomWord()
 
 
+def generate_word_easy():
+    return r.word(include_parts_of_speech=["nouns"], word_min_length=5, word_max_length=5)
 
-def generate_word():
+
+def generate_word_medium():
     return r.word(word_min_length=5, word_max_length=5)
 
-#print(generate_word())
+
+def generate_word_hard():
+    return r.word(word_min_length=5, word_max_length=6)
